712d0664395aaa449e03d4288c534662c88a5ccb
Aleksi Holappa <aleksi.holappa@protonmail.com>
2021-09-18 21:29:12 +0300

    better LSP setup, eslint + prettier to tsserver

--- init.lua
+++ init.lua
@@ -24,16 +24,12 @@ opt.visualbell = true
 opt.inccommand = "nosplit"
 opt.background = "dark"
 opt.autoread = true
-opt.listchars = { tab = "»·", space = "·" }
-vim.o.encoding = "utf8"
+opt.listchars = { tab = '»»', trail = '·' }
+vim.o.encoding = "utf-8"
 vim.o.completeopt = "menuone,noinsert,noselect"
 vim.g.forest_night_enable_italic = 1
 vim.g.forest_night_diagnostic_text_highlight = 1
 vim.g.mapleader = " "
-
--- Set chars to be displayed
--- vim.cmd [[set listchars=tab:»·,trail:·]]
-
 
 -- Packer stuff
 vim.cmd [[packadd packer.nvim]]
@@ -55,6 +51,12 @@ startup(function(use)
 
   -- syntax highlighting
   use "nvim-treesitter/nvim-treesitter"
+
+  -- Eslint for TS, JS etc
+  use {
+    "jose-elias-alvarez/nvim-lsp-ts-utils",
+    requires = {{'nvim-lua/plenary.nvim'}, {"jose-elias-alvarez/null-ls.nvim"}}
+  }
 
   -- file search
   use {
@@ -84,34 +86,17 @@ startup(function(use)
   -- Git blamer
   use "APZelos/blamer.nvim"
 
-  -- Indentline
-  use "Yggdroot/indentLine"
-
   -- Elixir Language support
   use "elixir-editors/vim-elixir"
 end)
 
 vim.g["completion_enable_snippet"] = "vim-vsnip"
+
 -- Theme
+vim.g["gruvbox_contrast_dark"] = "hard"
 vim.cmd([[colorscheme gruvbox]])
 
 -- LSP config
-local function setup_servers()
-  require'lspinstall'.setup()
-  local servers = require'lspinstall'.installed_servers()
-  for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
-  end
-end
-
-setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_servers() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
-
 local on_attach = function(client, bufnr)
   local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
@@ -124,8 +109,100 @@ local on_attach = function(client, bufnr)
   map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
   map('n', "ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", map_opts)
 
+  -- format on save
+  vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+
   require"completion".on_attach(client)
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local lspconfig = require("lspconfig")
+
+local function setup_servers()
+  require'lspinstall'.setup()
+  local servers = require'lspinstall'.installed_servers()
+  for _, server in pairs(servers) do
+    lspconfig[server].setup{
+      on_attach = on_attach,
+      capabilities = capabilities
+    }
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
+lspconfig.elixirls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "/home/aleksi/.local/share/nvim/lspinstall/elixir/./elixir-ls/language_server.sh" },
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = true,
+      fetchDeps = false
+    }
+  }
+})
+
+-- Custom on-attach for typescript since we also need eslint, prettier along with tsserver
+require("null-ls").config {}
+lspconfig["null-ls"].setup {}
+
+lspconfig.typescript.setup({
+  on_attach = function(client, bufnr) 
+    local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+    local map_opts = {noremap = true, silent = true}
+
+    map("n", "df", "<cmd>lua vim.lsp.buf.formatting()<cr>", map_opts)
+    map("n", "gc", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", map_opts)
+    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+    map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
+    map("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
+    map('n', "ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", map_opts)
+
+    local ts_utils = require("nvim-lsp-ts-utils")
+
+    -- disable tsserver formatting
+    client.resolved_capabilities.document_formatting = false
+
+    ts_utils.setup {
+      enable_import_on_completion = true,
+      eslint_bin = "eslint_d",
+      eslint_enable_diagnostics = true,
+      enable_formatting = true,
+      formatter = "prettier",
+      formatter_opts = {},
+    }
+
+    ts_utils.setup_client(client)
+
+    -- format on save
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+
+    require"completion".on_attach(client)
+  end,
+  capabilities = capabilities,
+  root_dir = function() return vim.loop.cwd() end
+})
+
+lspconfig.solargraph.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    solargraph = {
+      diagnostics = true,
+      completion = true
+    }
+  }
+})
 
 local eslint = {
   lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
@@ -136,142 +213,12 @@ local eslint = {
   formatStdin = true
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-local function eslint_config_exists()
-  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
-
-  if not vim.tbl_isempty(eslintrc) then
-    return true
-  end
-
-  if vim.fn.filereadable("package.json") then
-    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
-      return true
-    end
-  end
-
-  return false
-end
-
-local lspconfig = require("lspconfig")
-
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-
-lspconfig.terraform.setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-lspconfig.elixirls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  cmd = { "/home/aleksi/.local/share/nvim/lspinstall/elixir/./elixir-ls/language_server.sh" },
-  settings = {
-    elixirLS = {
-      dialyzerEnabled = true,
-      fetchDeps = false
-    }
-  }
-})
-
-lspconfig.solargraph.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-    solargraph = {
-      diagnostics = true,
-      completion = true
-    }
-  }
-})
-
-lspconfig.html.setup{
-  capabilities = capabilities,
-  on_attach = on_attach
-}
-
-lspconfig.efm.setup {
-  on_attach = on_attach,
-  root_dir = function()
-    if not eslint_config_exists() then
-      return nil
-    end
-    return vim.fn.getcwd()
-  end,
-  settings = {
-    languages = {
-      javascript = {eslint},
-      javascriptreact = {eslint},
-      ["javascript.jsx"] = {eslint},
-      typescript = {eslint},
-      ["typescript.tsx"] = {eslint},
-      typescriptreact = {eslint}
-    }
-  },
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescript.tsx",
-    "typescriptreact"
-  },
-}
-
+-- Setup autopairing
 require('nvim-autopairs').setup{}
 
-
+-- Tab through completion
 vim.cmd [[inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"]]
 vim.cmd [[inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"]]
-
--- Tab through LSP
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  --elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    --return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  --elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    --return t "<Plug>(vsnip-jump-prev)"
-  else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
 
 -- Telescope config
 vim.cmd [[nnoremap <leader>f <cmd>lua require('telescope.builtin').find_files()<cr>]]
@@ -279,11 +226,13 @@ vim.cmd [[nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<
 vim.cmd [[nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>]]
 vim.cmd [[nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>]]
 
-
 -- Treesitter
 local ts = require "nvim-treesitter.configs"
-ts.setup {ensure_installed = "maintained", indent = {enable = true}, highlight = {enable = true}}
-
+ts.setup {
+  ensure_installed = "maintained", 
+  indent = {enable = true}, 
+  highlight = {enable = true}
+}
 
 -- File Explorer
 vim.cmd [[nnoremap <leader>n :NvimTreeToggle<CR>]]
